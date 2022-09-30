5f71dcfeb93dda547cd834712ea454e34b25f5c6
Marilyn Thai <marilynthai15@gmail.com>
2022-09-30 12:02:01 -0700

    where we left off

---
+++ main.py
@@ -0,0 +1,15 @@
+from viewing_party.movie import Movie
+from viewing_party.person import Person
+
+
+Jerica = Person("Jerica",[Dune,Sandlot,Spiderman],["Netflix", "HBO"])
+Nancy = Person("Nancy",["Encanto","Sandlot","Spiderman"],["Netflix", "Disney+"])
+Auberon = Person("Auberon",["Encanto","Dune","Spiderman"],["Hulu", "HBO"])
+
+Dune = Movie("Dune","Science Fiction",4.8 ,"HBO")
+Sandlot = Movie("Sandlot","Family",4.0 ,"Disney+")
+Encanto = Movie("Encanto","Family",4.2 ,"Disney+")
+Spiderman = Movie("Spiderman","Action",4.0 ,"Netflix")
+
+
+
--- viewing_party/person.py
+++ viewing_party/person.py
@@ -1,3 +1,18 @@
 class Person:
-    pass
+    
+    def __init__(self,name,watched,subscriptions):
+        self.name = name
+        self.watched = watched
+        self.friends = []
+        self.subscriptions = subscriptions
+    
+    def add_watched(self,movie):
+        self.watched.append(movie)
+    
+    def get_num_watched(self):
+        return len(self.watched)
+    
+    def add_friends(self,friend):
+        self.friends.append(friend)
+
     
